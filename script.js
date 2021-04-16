14e1b670dcb6c884963c54948a1866b48c3f4749
Bharath Balasubramaniam <79153733+bharath-balasubramaniam@users.noreply.github.com>
2021-04-16 22:45:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,305 @@
+let data = [{"id":"1","name":"Milton Schaden","email":"Arthur_Grady@hotmail.com"},
+{"id":"2","name":"Daphne Wyman","email":"Erling.Marks18@yahoo.com"},
+{"id":"3","name":"Mortimer Reichel","email":"Sammy81@yahoo.com"},
+{"id":"4","name":"Fredrick Ward","email":"Perry.Mueller38@yahoo.com"},
+{"id":"5","name":"Dr. Meda Bradtke","email":"Maci33@hotmail.com"},
+{"id":"6","name":"Edna Wehner","email":"Zander.Will@yahoo.com"},
+{"id":"7","name":"Sam Boehm","email":"Carli_Hane34@yahoo.com"},
+{"id":"8","name":"Cooper DuBuque","email":"Karl_Mante@hotmail.com"},
+{"id":"9","name":"Queen Mayert","email":"Joanie_Lesch@yahoo.com"},
+{"id":"10","name":"Shanelle Gerlach","email":"Palma.Runolfsdottir@hotmail.com"},
+{"id":"11","name":"Maida Johns","email":"Josh_Hamill21@gmail.com"},
+{"id":"12","name":"Lauretta Conn I","email":"Allen.Mraz0@hotmail.com"},
+{"id":"13","name":"Morgan Ratke","email":"Petra.Denesik@hotmail.com"},
+{"id":"14","name":"Winifred Glover","email":"Jettie.Blanda@hotmail.com"},
+{"id":"15","name":"Eldora Armstrong","email":"Alex35@yahoo.com"},
+{"id":"16","name":"Mrs. Melyssa King","email":"Keely.Sauer@gmail.com"},
+{"id":"17","name":"Mrs. Leopoldo Hirthe","email":"Odessa_Mraz90@gmail.com"},
+{"id":"18","name":"Kaylin Kunde","email":"Jaron_Jacobson73@yahoo.com"},
+{"id":"19","name":"Ms. Aisha Kub","email":"Blaise23@yahoo.com"},
+{"id":"20","name":"Roderick Quigley","email":"Ed17@gmail.com"},
+{"id":"21","name":"Santiago Pfeffer Jr.","email":"Stuart.King45@yahoo.com"},
+{"id":"22","name":"Jordyn Wunsch","email":"Valentina22@hotmail.com"},
+{"id":"23","name":"Alvina Champlin","email":"Daryl93@hotmail.com"},
+{"id":"24","name":"Mittie Runolfsdottir","email":"Casimer.Renner@gmail.com"},
+{"id":"25","name":"Wilton Stokes","email":"Kacey86@yahoo.com"},
+{"id":"26","name":"Dr. Carole Lynch","email":"Melany0@yahoo.com"},
+{"id":"27","name":"Libbie Mann","email":"Otilia_Feest@hotmail.com"},
+{"id":"28","name":"Mayra Greenholt","email":"Wilburn84@gmail.com"},
+{"id":"29","name":"Miss Shanel Turner","email":"Herminia.Reilly@yahoo.com"},
+{"id":"30","name":"Brock Torphy","email":"Jackson_Rowe@gmail.com"},
+{"id":"31","name":"Josh Lesch","email":"Kristina2@yahoo.com"},
+{"id":"32","name":"Princess Roob","email":"Jammie.Muller@yahoo.com"},
+{"id":"33","name":"Kenyon Bogisich DVM","email":"Roy_Feil@yahoo.com"},
+{"id":"34","name":"Brady Schoen V","email":"Dan.Abshire58@gmail.com"},
+{"id":"35","name":"Russ Okuneva","email":"Britney_Jacobs@hotmail.com"},
+{"id":"36","name":"Carmen Heaney","email":"Frederik_Jaskolski93@yahoo.com"},
+{"id":"37","name":"Green Windler","email":"Maxine95@yahoo.com"},
+{"id":"38","name":"Lempi Kohler","email":"Declan64@yahoo.com"},
+{"id":"39","name":"Ike Kuvalis","email":"Verdie.Pagac@hotmail.com"},
+{"id":"40","name":"Emmitt O'Connell","email":"Bud.Greenholt@hotmail.com"},
+{"id":"41","name":"Lura Walker","email":"Marcella.Trantow@gmail.com"},
+{"id":"42","name":"Amira Zieme","email":"Fatima.Lockman@gmail.com"},
+{"id":"43","name":"Jettie Labadie","email":"Levi_Nikolaus61@hotmail.com"},
+{"id":"44","name":"Dominic Durgan","email":"Elta_Lakin@hotmail.com"},
+{"id":"45","name":"Cleora Toy","email":"Talia.Altenwerth18@gmail.com"},
+{"id":"46","name":"Wyman O'Keefe DDS","email":"Jaron62@yahoo.com"},
+{"id":"47","name":"Houston Morissette","email":"Edmond81@gmail.com"},
+{"id":"48","name":"Trystan Berge","email":"Arielle_Steuber35@gmail.com"},
+{"id":"49","name":"Maureen Johns","email":"Jenifer63@gmail.com"},
+{"id":"50","name":"Dr. Eloy Maggio","email":"Samanta.Tremblay@hotmail.com"},
+{"id":"51","name":"Claudine Windler","email":"Hilma_Bechtelar83@yahoo.com"},
+{"id":"52","name":"Lori Bogisich","email":"Laury.Legros68@gmail.com"},
+{"id":"53","name":"Cora Hirthe","email":"Arvilla_Reichel7@yahoo.com"},
+{"id":"54","name":"Edwina Turcotte","email":"Isidro.Breitenberg@hotmail.com"},
+{"id":"55","name":"Patience Gulgowski","email":"Patsy97@hotmail.com"},
+{"id":"56","name":"Shyann Tromp","email":"Marielle.Balistreri@gmail.com"},
+{"id":"57","name":"Hubert Turcotte","email":"Anabel.Nienow@yahoo.com"},
+{"id":"58","name":"Doris Altenwerth","email":"Buster.Rogahn77@yahoo.com"},
+{"id":"59","name":"Ms. Thora Stiedemann","email":"Emory_Jast@hotmail.com"},
+{"id":"60","name":"Gracie Osinski","email":"Helmer_Botsford@hotmail.com"},
+{"id":"61","name":"Karianne Kirlin PhD","email":"Mose27@yahoo.com"},
+{"id":"62","name":"Bernice Goldner","email":"Benjamin.Jones@yahoo.com"},
+{"id":"63","name":"Dr. Cordia Powlowski","email":"Zola35@hotmail.com"},
+{"id":"64","name":"Brenna Klein","email":"Rhea.Beer@hotmail.com"},
+{"id":"65","name":"Lily Ebert","email":"Rylee.Rogahn43@hotmail.com"},
+{"id":"66","name":"Kian Labadie I","email":"Elena_Gutmann@gmail.com"},
+{"id":"67","name":"Leif Swift","email":"Curtis.Lebsack66@hotmail.com"},
+{"id":"68","name":"Milford Schuster III","email":"Yesenia_Zieme71@hotmail.com"},
+{"id":"69","name":"Dr. Sheridan Hilll","email":"Blaze_Mayert14@hotmail.com"},
+{"id":"70","name":"Dorothy Wisoky","email":"Cordie.OHara@hotmail.com"},
+{"id":"71","name":"Meghan Cremin","email":"Arlie.DuBuque@gmail.com"},
+{"id":"72","name":"Daisha Paucek","email":"Alanis.Conroy@gmail.com"},
+{"id":"73","name":"Haylee Sawayn","email":"Natalie.Walter73@hotmail.com"},
+{"id":"74","name":"Kallie Langworth","email":"Ernestina.Rowe91@gmail.com"},
+{"id":"75","name":"Mrs. Megane Abernathy","email":"Carole22@hotmail.com"},
+{"id":"76","name":"Nellie Reynolds","email":"Rosalind21@gmail.com"},
+{"id":"77","name":"Alba Stehr","email":"Loyce13@hotmail.com"},
+{"id":"78","name":"Amaya Goyette","email":"Stefanie_Ledner@hotmail.com"},
+{"id":"79","name":"Herbert Brakus","email":"Ezekiel_Sawayn36@gmail.com"},
+{"id":"80","name":"Odie Blanda","email":"Kattie13@gmail.com"},
+{"id":"81","name":"Mrs. Susie Schaefer","email":"Norma68@hotmail.com"},
+{"id":"82","name":"Delaney Russel","email":"Casimir.Aufderhar@hotmail.com"},
+{"id":"83","name":"Quincy Baumbach MD","email":"Sarah.Rosenbaum@gmail.com"},
+{"id":"84","name":"Marietta Armstrong","email":"Alycia.Mueller48@yahoo.com"},
+{"id":"85","name":"Donato Hartmann","email":"Kenny56@hotmail.com"},
+{"id":"86","name":"Lenny Collins","email":"Bessie.Lebsack29@gmail.com"},
+{"id":"87","name":"Ronny Ankunding","email":"Emerson32@yahoo.com"},
+{"id":"88","name":"Eladio Mante","email":"Dennis.Schmitt73@yahoo.com"},
+{"id":"89","name":"Vidal Bogan","email":"Annetta.Howell62@yahoo.com"},
+{"id":"90","name":"Rod Upton","email":"Lucious_Greenholt@hotmail.com"},
+{"id":"91","name":"Dr. Thurman Hamill","email":"Mary68@yahoo.com"},
+{"id":"92","name":"Dr. Faustino Adams","email":"Salma_Cremin@hotmail.com"},
+{"id":"93","name":"Rosalia Denesik","email":"Quinten.Oberbrunner81@yahoo.com"},
+{"id":"94","name":"Cierra Langworth","email":"Adolf.Marks68@hotmail.com"},
+{"id":"95","name":"Mac Murphy V","email":"Sammy_Cormier18@gmail.com"},
+{"id":"96","name":"William Schmeler","email":"Sean_Buckridge56@hotmail.com"},
+{"id":"97","name":"Sharon Beahan","email":"Miller.Doyle6@yahoo.com"},
+{"id":"98","name":"Susana Breitenberg","email":"Jaylan_Boyer@hotmail.com"},
+{"id":"99","name":"Lamar Cummings","email":"Augusta_McGlynn15@hotmail.com"},
+{"id":"100","name":"Timothy Lynch","email":"Emanuel_Smitham@yahoo.com"}];
+
+let bdiv = document.createElement('div');
+bdiv.classList.add('container');
+let row1 = document.createElement('div');
+row1.classList.add('row');
+let col = document.createElement('div');
+col.classList.add('col-12');
+
+function tableElement(elem,elem_scope='') {
+    let element = document.createElement(elem);
+    element.setAttribute('scope',elem_scope);
+    return element;
+}
+
+let table = tableElement('table');
+table.classList.add('table');
+
+let thead = tableElement('thead');
+thead.classList.add('thead-dark');
+
+let tr0 = tableElement('tr');
+let th1 = tableElement('th','col');
+th1.innerHTML = 'User id';
+let th2 = tableElement('th','col');
+th2.innerHTML = 'Name';
+let th3 = tableElement('th','col');
+th3.innerHTML = 'E-mail';
+tr0.append(th1,th2,th3)
+thead.append(tr0);
+
+let t_body = tableElement('tbody');
+
+function tbody(trth,trtd1,trtd2) {
+    let tr1 = tableElement('tr');
+    let tr1th = tableElement('th','row');
+    tr1th.id = trth;
+    let tr1td1 = tableElement('td');
+    tr1td1.id = trtd1;
+    let tr1td2 = tableElement('td');
+    tr1.append(tr1th,tr1td1,tr1td2);
+    tr1td2.id = trtd2;
+    return tr1;
+}
+
+let tr1 = tbody('tr1th','tr1td1','tr1td2');
+let tr2 = tbody('tr2th','tr2td1','tr2td2');
+let tr3 = tbody('tr3th','tr3td1','tr3td2');
+let tr4 = tbody('tr4th','tr4td1','tr4td2');
+let tr5 = tbody('tr5th','tr5td1','tr5td2');
+let tr6 = tbody('tr6th','tr6td1','tr6td2');
+let tr7 = tbody('tr7th','tr7td1','tr7td2');
+let tr8 = tbody('tr8th','tr8td1','tr8td2');
+let tr9 = tbody('tr9th','tr9td1','tr9td2');
+let tr10 = tbody('tr10th','tr10td1','tr10td2');
+
+t_body.append(tr1,tr2,tr3,tr4,tr5,tr6,tr7,tr8,tr9,tr10);
+table.append(thead,t_body);
+
+function btn(btn) {
+    let button = document.createElement(btn);
+    button.setAttribute('type','button');
+    button.setAttribute('class','btn')
+    button.classList.add('btn-outline-dark');
+    return button;
+}
+let buttonmain = document.createElement('div');
+buttonmain.classList.add('btn-toolbar');
+buttonmain.setAttribute('role','toolbar');
+buttonmain.setAttribute('aria-label','Toolbar with button groups')
+
+let btndiv = document.createElement('div');
+btndiv.setAttribute('class','btn-group');
+btndiv.classList.add('mr-2');
+btndiv.setAttribute('role','group');
+btndiv.setAttribute('aria-label','First group');
+
+let btnfirst = btn('button');
+btnfirst.innerHTML = 'First';
+btnfirst.setAttribute('onclick','foo(data,0,9)')
+let btnprevious = btn('button');
+btnprevious.innerHTML = 'Previous';
+btnprevious.setAttribute('onclick','foo2()')
+btndiv.append(btnfirst,btnprevious);
+
+
+
+let btndiv2 = document.createElement('div');
+btndiv2.setAttribute('class','btn-group');
+btndiv2.classList.add('mr-2');
+btndiv2.setAttribute('role','group');
+btndiv2.setAttribute('aria-label','second group');
+
+let btn2 = btn('button');
+btn2.innerHTML = '2';
+btn2.setAttribute('onclick','foo(data,10,19)');
+let btn3 = btn('button');
+btn3.innerHTML = '3';
+btn3.setAttribute('onclick','foo(data,20,29)');
+let btn4 = btn('button');
+btn4.innerHTML = '4';
+btn4.setAttribute('onclick','foo(data,30,39)')
+let btn5 = btn('button');
+btn5.innerHTML = '5';
+btn5.setAttribute('onclick','foo(data,40,49)')
+let btn6 = btn('button');
+btn6.innerHTML = '6';
+btn6.setAttribute('onclick','foo(data,50,59)')
+let btn7 = btn('button');
+btn7.innerHTML = '7';
+btn7.setAttribute('onclick','foo(data,60,69)')
+let btn8 = btn('button');
+btn8.innerHTML = '8';
+btn8.setAttribute('onclick','foo(data,70,79)')
+let btn9 = btn('button');
+btn9.innerHTML = '9';
+btn9.setAttribute('onclick','foo(data,80,89)')
+btndiv2.append(btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9);
+
+let btndiv3 = document.createElement('div');
+btndiv3.setAttribute('class','btn-group');
+btndiv3.classList.add('mr-2');
+btndiv3.setAttribute('role','group');
+btndiv3.setAttribute('aria-label','Third group');
+
+let btnnext = btn('button');
+btnnext.innerHTML = 'Next';
+btnnext.setAttribute('onclick','foo1()');
+let btnlast = btn('button');
+btnlast.innerHTML = 'Last';
+btnlast.setAttribute('onclick','foo(data,90,99)')
+
+btndiv3.append(btnnext,btnlast);
+
+buttonmain.append(btndiv,btndiv2,btndiv3);
+let row2 = document.createElement('div');
+row2.classList.add('row');
+row2.append(buttonmain);
+
+
+col.append(table);
+row1.append(col);
+bdiv.append(row1,row2);
+document.body.append(bdiv);
+
+function foo(data,i=0,j=9) {
+    let id = 1
+    for( i; i <= j; i++,id++) {
+        let b = document.getElementById(`tr${id}th`)
+        b.innerText = data[i].id;
+        let c = document.getElementById(`tr${id}td1`)
+        c.innerText = data[i].name;
+        let d = document.getElementById(`tr${id}td2`)
+        d.innerText = data[i].email;
+    }
+}
+foo(data)
+
+function foo1() {
+    let a = document.getElementById(`tr5th`).innerHTML;
+    if(a<10){
+        foo(data,10,19);
+    }else if(a<20) {
+        foo(data,20,29);
+    }else if(a<30) {
+        foo(data,30,39);
+    }else if(a<40) {
+        foo(data,40,49);
+    }else if(a<50) {
+        foo(data,50,59);
+    }else if(a<60) {
+        foo(data,60,69);
+    }else if(a<70) {
+        foo(data,70,79);
+    }else if(a<80) {
+        foo(data,80,89);
+    }else if(a<90) {
+        foo(data,90,99);
+    }else if(a<100) {
+        foo(data,90,99);
+    }
+}
+function foo2() {
+    let a = document.getElementById(`tr5th`).innerHTML;
+    if(a<10){
+        foo(data,0,9);
+    }else if(a<20) {
+        foo(data,0,9);
+    }else if(a<30) {
+        foo(data,10,19);
+    }else if(a<40) {
+        foo(data,20,29);
+    }else if(a<50) {
+        foo(data,30,39);
+    }else if(a<60) {
+        foo(data,40,49);
+    }else if(a<70) {
+        foo(data,50,59);
+    }else if(a<80) {
+        foo(data,60,69);
+    }else if(a<90) {
+        foo(data,70,79);
+    }else if(a<100) {
+        foo(data,80,89);
+    }
+}
